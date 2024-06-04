a76416c0d1e8b6630b0c31ff4b338dab3667be3e
Austin Thesing <austin@designxdevelop.com>
2024-06-03 18:19:22 -0600

    Add text styles to link

--- ms-form-route.js
+++ ms-form-route.js
@@ -7,7 +7,7 @@ document.querySelector("[data-form='submit-btn']").addEventListener("click", fun
 
     // Update the success message immediately
     successMessageElement.innerHTML =
-      'Each and every business matters to us, and we\'re eager to hear more about yours. In 5 seconds, you will be redirected to <a href="https://app.onrampfunds.com/pre-approval">https://app.onrampfunds.com/pre-approval</a>Where you can get your no commitment, pre-approval offer.';
+      'Each and every business matters to us, and we\'re eager to hear more about yours. <br>In 5 seconds, you will be redirected to <a class="desktop-p2" href="https://app.onrampfunds.com/pre-approval">https://app.onrampfunds.com/pre-approval</a></br>Where you can get your no commitment, pre-approval offer.';
 
     // Then set the timeout for redirection
     setTimeout(function () {
